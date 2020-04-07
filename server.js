4dd6954c11e1cadf0f8991e7d076040a7c5a3b7e
Ben Barnett <ben@univthink.com>
2020-04-07 10:41:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ app.get('/token', function (req, res) {
 app.use(express.static(publicDir));
 
 console.log('Listening at', publicDir, hostname, port);
-app.listen(port, hostname);
+app.listen(8080);
